0ec433f766eb5c17538ad8359fca2d04ba8551cd
Matheus <mathugolini@gmail.com>
2023-07-24 17:47:43 -0300

    Adicionando regra de negocio

--- force-app/main/default/lwc/login/login.js
+++ force-app/main/default/lwc/login/login.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Login extends LightningElement {}
+export default class Login extends LightningElement {
+  @track showRequiredMessage = true;
+
+  handleLogin() {
+    const emailInput = this.template.querySelector('input[name="email"]');
+    const passwordInput = this.template.querySelector('input[name="password"]');
+
+    if (!emailInput.value || !passwordInput.value) {
+      this.showRequiredMessage = false;
+    } else {
+      this.showRequiredMessage = true;
+      // Restante da lógica de login
+    }
+  }
+}
