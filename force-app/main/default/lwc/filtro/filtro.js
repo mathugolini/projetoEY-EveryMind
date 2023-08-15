746ec5a586dbb628cc4e4ba0cbdfcf837f441031
Matheus <mathugolini@gmail.com>
2023-08-15 15:39:11 -0300

    Importando static resources

--- force-app/main/default/lwc/filtro/filtro.js
+++ force-app/main/default/lwc/filtro/filtro.js
@@ -1,6 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class Filtro extends LightningElement {}
+
+
+
+import 	EYassets from '@salesforce/resourceUrl/EYassets'
+
+export default class Filtro extends LightningElement {
+
+   
+    candidato1 = `${EYassets}/EYassets/matheusLogo.png`
+    candidato2 = `${EYassets}/EYassets/Cahue.jpg`
+    candidato3 = `${EYassets}/EYassets/Gabriel.jpg`
+    candidato4 = `${EYassets}/EYassets/Gustavo.jpg`
+
+}
 
 
 
