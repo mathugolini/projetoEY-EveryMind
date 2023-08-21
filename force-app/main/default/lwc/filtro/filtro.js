17f9d4c2ed0ce10e64c1e56120d14a8db82e48f8
Matheus <mathugolini@gmail.com>
2023-08-21 14:29:25 -0300

    Referencia aos recursos estaticos implementados

--- force-app/main/default/lwc/filtro/filtro.js
+++ force-app/main/default/lwc/filtro/filtro.js
@@ -1,11 +1,17 @@
 import { LightningElement } from 'lwc';
-import EYassets from '@salesforce/resourceUrl/EYassets';
+
+import EYassetsp from '@salesforce/resourceUrl/EYassetsp';
 
 export default class Filtro extends LightningElement {
-    candidato1 = `${EYassets}/EYassets/matheusLogo.png`;
-    candidato2 = `${EYassets}/EYassets/Cahue.jpg`;
-    candidato3 = `${EYassets}/EYassets/Gabriel.jpg`;
-    candidato4 = `${EYassets}/EYassets/Gustavo.jpg`;
+    candidato1 = `${EYassetsp}/EYassets/matheusLogo.png`;
+    candidato2 = `${EYassetsp}/EYassets/Cahue.jpg`;
+    candidato3 = `${EYassetsp}/EYassets/Gabriel.jpg`;
+    candidato4 = `${EYassetsp}/EYassets/Gustavo.jpg`;
+    candidato5 = `${EYassetsp}/EYassets/Vinicius.jpg`;
+    candidato6 = `${EYassetsp}/EYassets/Sofia.jpg`;
+    candidato7 = `${EYassetsp}/EYassets/Ricardo.jpg`;
+    candidato8 = `${EYassetsp}/EYassets/Laura.jpg`;
+    
     searchText = '';
 
     handleSearchKeyUp(event) {
