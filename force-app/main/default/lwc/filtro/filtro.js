0973c2b8dacb7b9afa1e48ecc94c107db27a2b3b
Matheus <mathugolini@gmail.com>
2023-08-25 13:46:08 -0300

    Refatorando novos static resources

--- force-app/main/default/lwc/filtro/filtro.js
+++ force-app/main/default/lwc/filtro/filtro.js
@@ -1,16 +1,16 @@
 import { LightningElement } from 'lwc';
 
-import EYassetsp from '@salesforce/resourceUrl/EYassetsp';
+import Everymindassets from '@salesforce/resourceUrl/Everymindassets';
 
 export default class Filtro extends LightningElement {
-    candidato1 = `${EYassetsp}/EYassets/matheusLogo.png`;
-    candidato2 = `${EYassetsp}/EYassets/Cahue.jpg`;
-    candidato3 = `${EYassetsp}/EYassets/Gabriel.jpg`;
-    candidato4 = `${EYassetsp}/EYassets/Gustavo.jpg`;
-    candidato5 = `${EYassetsp}/EYassets/Vinicius.jpg`;
-    candidato6 = `${EYassetsp}/EYassets/Sofia.jpg`;
-    candidato7 = `${EYassetsp}/EYassets/Ricardo.jpg`;
-    candidato8 = `${EYassetsp}/EYassets/Laura.jpg`;
+    candidato1 = `${Everymindassets}/EYassets/matheusLogo.png`;
+    candidato2 = `${Everymindassets}/EYassets/Cahue.jpg`;
+    candidato3 = `${Everymindassets}/EYassets/Gabriel.jpg`;
+    candidato4 = `${Everymindassets}/EYassets/Gustavo.jpg`;
+    candidato5 = `${Everymindassets}/EYassets/Vinicius.jpg`;
+    candidato6 = `${Everymindassets}/EYassets/Sofia.jpg`;
+    candidato7 = `${Everymindassets}/EYassets/Ricardo.jpg`;
+    candidato8 = `${Everymindassets}/EYassets/Laura.jpg`;
     
     searchText = '';
 
