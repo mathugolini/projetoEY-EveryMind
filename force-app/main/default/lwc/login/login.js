e26e99e94ba41be300ad716c5b8557614b0bf91d
Matheus <mathugolini@gmail.com>
2023-08-28 20:53:12 -0300

    Adicionando static resources de referencia js

--- force-app/main/default/lwc/login/login.js
+++ force-app/main/default/lwc/login/login.js
@@ -1,7 +1,12 @@
 import { LightningElement, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import Everymindassets from '@salesforce/resourceUrl/Everymindassets';
 
 export default class Login extends NavigationMixin(LightningElement) {
+
+  everymindlogo = `${Everymindassets}/EYassets/everymind.png`;
+  modelo = `${Everymindassets}/EYassets/modelo.png`;
+
   @track showRequiredMessage = true;
   @track showSuccessMessage = false;
 
