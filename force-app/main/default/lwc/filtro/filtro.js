caf2660a8df6c17a7f2a57416d27080174ae997d
Matheus <mathugolini@gmail.com>
2023-08-29 11:50:09 -0300

    Adequando escopo para projeto every mind

--- force-app/main/default/lwc/filtro/filtro.js
+++ force-app/main/default/lwc/filtro/filtro.js
@@ -31,15 +31,20 @@ export default class Filtro extends LightningElement {
     sendQuestion() {
         var searchInput = "Analise os currículos e responda apenas com o nome dos candidatos mais adequados:"
         searchInput += this.curriculums();
-        searchInput += this.template.querySelector('input[name="search"]').value;
+        searchInput += this.template.querySelector('input[name="search"]').value + '\n';
+        searchInput += "Responda apenas com o nome dos candidatos!"
 
-        const OPENAI_API_KEY = "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";
+        const OPENAI_API_KEY = "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";
 
         const candidatos = {};
-        candidatos["matheus"] = this.template.querySelector('div[class="matheus"]');
-        candidatos["cahue"] = this.template.querySelector('div[class="cahue"]');
-        candidatos["gabriel"] = this.template.querySelector('div[class="gabriel"]');
-        candidatos["gustavo"] = this.template.querySelector('div[class="gustavo"]');
+        candidatos["matheus"] = this.template.querySelector('div[class="candidato-1"]');
+        candidatos["cahue"] = this.template.querySelector('div[class="candidato-2"]');
+        candidatos["gabriel"] = this.template.querySelector('div[class="candidato-3"]');
+        candidatos["gustavo"] = this.template.querySelector('div[class="candidato-4"]');
+        candidatos["vinicius"] = this.template.querySelector('div[class="candidato-5"]');
+        candidatos["sofia"] = this.template.querySelector('div[class="candidato-6"]');
+        candidatos["ricardo"] = this.template.querySelector('div[class="candidato-7"]');
+        candidatos["laura"] = this.template.querySelector('div[class="candidato-8"]');
         
         fetch("https://api.openai.com/v1/chat/completions", {
             method: "POST",
@@ -61,20 +66,26 @@ export default class Filtro extends LightningElement {
                 console.log(`Error: ${json.error.message}`);
             } else if (json.choices?.[0].message) {
                 var text = json.choices[0].message.content || "Sem resposta";
-                const responseSplit = text.replaceAll("e ", ',').split(",");
+                const responseSplit = text.replaceAll(" e ", ', ').replace(".", "").split(", ");
                 console.log(text);
+                console.log(responseSplit[0]);
                 console.log(responseSplit[1]);
                 
                 for (var key in candidatos) {
-                    if (key != text.toLowerCase())
-                        candidatos[key].style.display = 'none';
+                    candidatos[key].style.display = 'none';
+                    for (var resp in responseSplit) {
+                        if (key == responseSplit[resp].toLowerCase()) {
+                            candidatos[key].style.display = 'inline';
+                            break;
+                        }
+                    }
                 }
 
             }
         })
         .catch((error) => console.error("Error:", error))
         .finally(() => {
-            console.log('finally')
+            //console.log('finally')
         });
         
     }
@@ -84,6 +95,9 @@ export default class Filtro extends LightningElement {
         curriculums += "Currículo Cahue: \n Desenvolvedor de Aplicações \n Minha exposição internacional em uma idade jovem me deu uma profunda experiência cobrindo vários aspectos da vida profissional, como atendimento ao cliente, vendas e estratégia. De volta ao Brasil desde 2019, iniciei meus estudos e carreira na Tecnologia da Informação. Atualmente atuando como iOS Developer Trainee, utilizando a linguagem de programação Swift (Storyboard, UIKit, Auto Layout, ViewCode e MVC Design Pattern) e a IDE XCode com 2 anos de experiência. \n";
         curriculums += "Currículo Gabriel: \n Desenvolvedor de Aplicações \n Experiência como Desenvolvedor e Quality Assurance, atuando em projetos para empresas na área de jogos, com 3 anos de experiência. Aprimorando minhas habilidades para atender as necessidades do cliente e auxiliar a equipe, utilizando metodologias ágeis. Com sólida experiência internacional e Inglês fluente. Especialidade em C#, Java e Cloud. \n"
         curriculums += "Currículo Gustavo: \n Analista de banco de dados \n Experiência como DBA, atuando em projetos para empresas na área de telecomunicações e petrolífera por 5 anos. Conhecimentos avançados em SQL e banco de dados Oracle, além de data warehousing."
+        curriculums += "Currículo Sofia: \n Designer UX \n Designer dedicada a criar experiências digitais envolventes e intuitivas. Minha paixão pela psicologia cognitiva me ajuda a compreender as necessidades dos usuários e a traduzi-las em interfaces amigáveis. Tenho experiência em colaborar com equipes de desenvolvimento ágil para criar produtos digitais visualmente atraentes e funcionais. \n"
+        curriculums += "Currículo Ricardo: \n Engenheiro de Controle e Automação \n Engenheiro apaixonado por automação industrial e controle de processos. Com experiência em projetar, implementar e otimizar sistemas de automação para indústrias de manufatura. Forte histórico em integração de PLCs, programação de CLPs e supervisórios. Busco constantemente aplicar tecnologias emergentes para melhorar a eficiência e a qualidade dos processos industriais. \n"
+        curriculums += "Currículo Laura: \n Analista de Dados \n Profissional com vasta experiência em análise de dados e criação de visualizações impactantes. Trabalhei em diversos projetos multidisciplinares, colaborando com equipes para extrair insights acionáveis de conjuntos de dados complexos. Minha abordagem orientada a resultados e habilidades avançadas em ferramentas de visualização, como Tableau e Power BI, me permitem transformar dados em narrativas claras. \n"
         return curriculums;
     }
 
