2093dd4214508187210ff8f11aec2248f44273af
Matheus <mathugolini@gmail.com>
2023-08-30 16:08:42 -0300

    Novo static resources e estilização

--- force-app/main/default/lwc/home/home.js
+++ force-app/main/default/lwc/home/home.js
@@ -1,17 +1,24 @@
 import { LightningElement, track, wire } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation'; // Certifique-se de importar corretamente
 import Everymindassets from '@salesforce/resourceUrl/Everymindassets';
+import Everymindassets2 from '@salesforce/resourceUrl/Everymindassets2';
 
 export default class Home extends NavigationMixin(LightningElement) {
 
+    telefone = `${Everymindassets2}/EYassets/telefone.png`;
+    seta = `${Everymindassets2}/EYassets/seta.png`;
+    info = `${Everymindassets2}/EYassets/info.png`;
+    conquista = `${Everymindassets2}/EYassets/conquista.png`;
+    caixa = `${Everymindassets2}/EYassets/caixa.png`;
     everymindlogo = `${Everymindassets}/EYassets/everymind.png`;
+
     @track menuItems = [
-        { id: 'item1', link: 'https://www.everymind.com.br/?utm_source=ISG&utm_medium=LandingPage%20&utm_id=Institucional%20', icon: `${Everymindassets}/EYassets/matheusLogo.png`, style: '' },
-        { id: 'item2', link: '#', icon: 'icon2.png', style: '' },
-        { id: 'item3', link: '#', icon: 'icon3.png', style: '' },
-        { id: 'item4', link: '#', icon: 'icon4.png', style: '' },
-        { id: 'item5', link: '#', icon: 'icon5.png', style: '' },
-        { id: 'item6', link: '#', icon: 'icon6.png', style: '' },
+        { id: 'item1', link: 'https://www.everymind.com.br/?utm_source=ISG&utm_medium=LandingPage%20&utm_id=Institucional%20', icon: `${Everymindassets}/EYassets/everymind.png`, style: '' },
+        { id: 'item2', link: '#', icon: `${Everymindassets2}/EYassets/info.png`, style: '' },
+        { id: 'item3', link: '#', icon: `${Everymindassets2}/EYassets/caixa.png`, style: '' },
+        { id: 'item4', link: '#', icon: `${Everymindassets2}/EYassets/conquista.png`, style: '' },
+        { id: 'item5', link: '#', icon: `${Everymindassets2}/EYassets/telefone.png`, style: '' },
+        { id: 'item6', link: '#', icon: `${Everymindassets2}/EYassets/seta.png`, style: '' },
     ];
 
     isExpanded = false;
